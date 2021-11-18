ee76be2e50824a2c09bd71b3d810d39401474637
KonradWrbl <160815@stud.prz.edu.pl>
2021-11-18 14:35:35 +0100

    fix(common): unused files removed

--- libs/todo-app/feature-add-edit-todo/src/lib/todo-app-feature-add-edit-todo.module.ts
+++ libs/todo-app/feature-add-edit-todo/src/lib/todo-app-feature-add-edit-todo.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddEditDialogComponent } from './add-edit-dialog/add-edit-dialog.component';
-import { AddEditFormComponent } from './add-edit-form/add-edit-form.component';
 import { SharedMaterialModule } from '@todo-app/shared/material';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TodoAppDataAccessModule } from '@todo-app/todo-app/data-access';
@@ -10,7 +9,6 @@ import { TodoAppDataAccessModule } from '@todo-app/todo-app/data-access';
   imports: [CommonModule, FormsModule, ReactiveFormsModule, SharedMaterialModule, TodoAppDataAccessModule ],
   declarations: [
     AddEditDialogComponent,
-    AddEditFormComponent
   ],
 })
 export class TodoAppFeatureAddEditTodoModule {}
